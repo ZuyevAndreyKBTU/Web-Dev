bde629c28fe1c590a155f0c63ebd6e70877b5f62
ZuyevAndreyKBTU <158192484+ZuyevAndreyKBTU@users.noreply.github.com>
2024-02-21 21:52:56 +0600

    Add files via upload

---
+++ Lab4/src/app/products.ts
@@ -0,0 +1,115 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  rating: number;
+  image_url: string;
+  url: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Memo DL05 черный',
+    price: 6791,
+    description:
+      'интерфейс: USB Type-C скорость вентиляторов: 8000.0 об/мин шум: 32.0 дБ подсветка: Да',
+    rating: 4.5,
+    image_url: 'https://resources.cdn-kaspi.kz/img/m/p/h89/h6f/70076537307166.jpg?format=gallery-medium',
+    url: 'https://kaspi.kz/shop/p/memo-dl05-chernyi-105898830/?c=750000000'
+  },
+  {
+    id: 2,
+    name: 'Coolingpad H-15 серый',
+    price: 4889,
+    description:
+      'интерфейс: USB 2.0 скорость вентиляторов: 6496.0 об/мин подсветка: Нет',
+    rating: 5,
+    image_url: 'https://resources.cdn-kaspi.kz/img/m/p/h59/h05/64230409928734.jpg?format=gallery-medium',
+    url: 'https://kaspi.kz/shop/p/coolingpad-h-15-seryi-106786206/?c=750000000'
+  },
+  {
+    id: 3,
+    name: 'FS01 черный',
+    price: 6990,
+    description: 'интерфейс: USB Type-C подсветка: Да',
+    rating: NaN,
+    image_url: 'https://resources.cdn-kaspi.kz/img/m/p/h4e/hc4/67248042704926.jpg?format=gallery-medium',
+    url: 'https://kaspi.kz/shop/p/fs01-chernyi-116309612/?c=750000000'
+  },
+  {
+    id: 4,
+    name: 'DL01 черный',
+    price: 3989,
+    description:
+      'интерфейс: USB Type-C скорость вентиляторов: 7500.0 об/мин подсветка: Нет',
+      rating: 4,
+      image_url: 'https://resources.cdn-kaspi.kz/img/m/p/h88/h0b/85080254939166.png?format=gallery-medium',
+      url: 'https://kaspi.kz/shop/p/dl01-chernyi-108432310/?c=750000000'
+  },
+  {
+    id: 5,
+    name: 'Black Shark FunCooler 3 Pro черный',
+    price: 19496,
+    description: '',
+    rating: 5,
+    image_url: 'https://resources.cdn-kaspi.kz/img/m/p/h88/h4e/84676190830622.jpg?format=gallery-medium',
+    url: 'https://kaspi.kz/shop/p/black-shark-funcooler-3-pro-chernyi-106059798/?c=750000000'
+  },
+  {
+    id: 6,
+    name: 'Memo DL05 черный',
+    price: 6172,
+    description:
+      'интерфейс: USB Type-C скорость вентиляторов: 8500.0 об/мин подсветка: Да',
+    rating: 5,
+    image_url: 'https://resources.cdn-kaspi.kz/img/m/p/hcb/hf8/64501718745118.jpg?format=gallery-medium',
+    url: 'https://kaspi.kz/shop/p/memo-dl05-chernyi-115211193/?c=750000000'
+  },
+  {
+    id: 7,
+    name: 'L10 черный',
+    price: 9068,
+    description: 'интерфейс: USB Type-C подсветка: Да',
+    rating: 4.5,
+    image_url: 'https://resources.cdn-kaspi.kz/img/m/p/h69/ha8/67857210703902.jpg?format=gallery-medium',
+    url: 'https://kaspi.kz/shop/p/l10-chernyi-112051018/?c=750000000'
+  },
+  {
+    id: 8,
+    name: 'Nubia R6 RedMagic PA3103 черный',
+    price: 15500,
+    description:
+      'интерфейс: USB Type-C скорость вентиляторов: 7200.0 об/мин подсветка: Нет',
+    rating: 4.5,
+    image_url: 'https://resources.cdn-kaspi.kz/img/m/p/ha0/h8a/85045563097118.jpg?format=gallery-medium',
+    url: 'https://kaspi.kz/shop/p/nubia-r6-redmagic-pa3103-chernyi-108201385/?c=750000000'
+  },
+  {
+    id: 9,
+    name: 'Black Shark Magnetic Cooler черный',
+    price: 15999,
+    description:
+      'интерфейс: USB 2.0 скорость вентиляторов: 6496.0 об/мин подсветка: Нет',
+    rating: 4.5,
+    image_url: 'https://resources.cdn-kaspi.kz/img/m/p/h77/h5c/64858501480478.jpg?format=gallery-medium',
+    url: 'https://kaspi.kz/shop/p/black-shark-magnetic-cooler-chernyi-102715319/?c=750000000'
+  },
+  {
+    id: 10,
+    name: 'X52 серый',
+    price: 2860,
+    description:
+      'интерфейс: USB Type-C скорость вентиляторов: 8000.0 об/мин шум: 32.0 дБ подсветка: Да',
+    rating: 4.5,
+    image_url: 'https://resources.cdn-kaspi.kz/img/m/p/he3/h36/65023053135902.jpg?format=gallery-medium',
+    url: 'https://kaspi.kz/shop/p/x52-seryi-109351719/?c=750000000'
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
